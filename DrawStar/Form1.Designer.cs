b6ee3e00f6ae853c5bb5a7c19a7192ee09c0ff6b
nichpuet <napuetz@gmail.com>
2017-11-27 13:35:23 -0500

    Add project files.

---
+++ DrawStar/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DrawStar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.yInput = new System.Windows.Forms.TextBox();
+            this.sizeInput = new System.Windows.Forms.TextBox();
+            this.redRadio = new System.Windows.Forms.RadioButton();
+            this.blueRadio = new System.Windows.Forms.RadioButton();
+            this.greenRadio = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(12, 12);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "Draw Star";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(93, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input your values and press draw Star to draw the star";
+            // 
+            // xInput
+            // 
+            this.xInput.Location = new System.Drawing.Point(43, 41);
+            this.xInput.Name = "xInput";
+            this.xInput.Size = new System.Drawing.Size(25, 20);
+            this.xInput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Size:";
+            // 
+            // yInput
+            // 
+            this.yInput.Location = new System.Drawing.Point(43, 70);
+            this.yInput.Name = "yInput";
+            this.yInput.Size = new System.Drawing.Size(25, 20);
+            this.yInput.TabIndex = 6;
+            // 
+            // sizeInput
+            // 
+            this.sizeInput.Location = new System.Drawing.Point(43, 96);
+            this.sizeInput.Name = "sizeInput";
+            this.sizeInput.Size = new System.Drawing.Size(25, 20);
+            this.sizeInput.TabIndex = 7;
+            // 
+            // redRadio
+            // 
+            this.redRadio.AutoSize = true;
+            this.redRadio.Location = new System.Drawing.Point(15, 134);
+            this.redRadio.Name = "redRadio";
+            this.redRadio.Size = new System.Drawing.Size(67, 17);
+            this.redRadio.TabIndex = 8;
+            this.redRadio.TabStop = true;
+            this.redRadio.Text = "Red Pen";
+            this.redRadio.UseVisualStyleBackColor = true;
+            // 
+            // blueRadio
+            // 
+            this.blueRadio.AutoSize = true;
+            this.blueRadio.Location = new System.Drawing.Point(15, 157);
+            this.blueRadio.Name = "blueRadio";
+            this.blueRadio.Size = new System.Drawing.Size(68, 17);
+            this.blueRadio.TabIndex = 9;
+            this.blueRadio.TabStop = true;
+            this.blueRadio.Text = "Blue Pen";
+            this.blueRadio.UseVisualStyleBackColor = true;
+            // 
+            // greenRadio
+            // 
+            this.greenRadio.AutoSize = true;
+            this.greenRadio.Location = new System.Drawing.Point(15, 180);
+            this.greenRadio.Name = "greenRadio";
+            this.greenRadio.Size = new System.Drawing.Size(76, 17);
+            this.greenRadio.TabIndex = 10;
+            this.greenRadio.TabStop = true;
+            this.greenRadio.Text = "Green Pen";
+            this.greenRadio.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.greenRadio);
+            this.Controls.Add(this.blueRadio);
+            this.Controls.Add(this.redRadio);
+            this.Controls.Add(this.sizeInput);
+            this.Controls.Add(this.yInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.xInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox xInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox yInput;
+        private System.Windows.Forms.TextBox sizeInput;
+        private System.Windows.Forms.RadioButton redRadio;
+        private System.Windows.Forms.RadioButton blueRadio;
+        private System.Windows.Forms.RadioButton greenRadio;
+    }
+}
+
---
+++ DrawStar/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawStar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            float xValue, yValue, sizeValue;
+
+            xValue = Convert.ToSingle(xInput);
+            yValue = Convert.ToSingle(yInput);
+            sizeValue = Convert.ToSingle(sizeInput);
+
+            Pen DrawPen;
+
+            if (redRadio.Checked)
+            {
+                DrawPen = new Pen(Color.Red);
+            }
+            else if (blueRadio.Checked)
+            {
+                DrawPen = new Pen(Color.Blue);
+            }
+            else
+            {
+                DrawPen = new Pen(Color.Green);
+            }
+        }
+    }
+}
